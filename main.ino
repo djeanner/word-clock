ba1edc64e069aa4738a0e9a95ed1899619051164
djeanner <djeanner@users.noreply.github.com>
2026-02-09 22:15:30 +0100

    refactoring: moves all relevant code into WordClock class

--- main.ino
+++ main.ino
@@ -79,169 +79,195 @@ bool debug2 = !debug8;       // front display debugging steps
 const bool debug3 = true;    // dump info about the validation process of times
 
 
-// HERE define class WordClock
-enum class WordGP : uint8_t {
-  H1_ = 0,
-  H2_ = 1,
-  H3_ = 2,
-  H4_ = 3,
-  H5_ = 9,
-  H6_ = 5,
-  H7_ = 6,
-  H8_ = 7,
-  H9_ = 8,
-  H10 = 4,
-  H11 = 10,
-  H12 = 11,
-  // PM        = 12 
-  // AM        = 13
-  HTO = 14,
-  HPAST = 15,
-  HALF = 16,
-  M10 = 17,
-  M5 = 18,
-  MITIS = 12,  // NEW alwas on
-  MOCLOCK = 19,
-  MQUARTER = 20,
-  MTWENTY = 13,     // was 22
-  EXTRA = 21,       // new
-};
+class WordClock {
+private:
+const size_t totOutputPins;
+size_t *values;
+public:
 
-const size_t totOutputPins = 22;
-size_t values[totOutputPins];
+  enum class WordGP : uint8_t {
+    H1_ = 0,
+    H2_ = 1,
+    H3_ = 2,
+    H4_ = 3,
+    H5_ = 9,
+    H6_ = 5,
+    H7_ = 6,
+    H8_ = 7,
+    H9_ = 8,
+    H10 = 4,
+    H11 = 10,
+    H12 = 11,
+    // PM        = 12 
+    // AM        = 13
+    HTO = 14,
+    HPAST = 15,
+    HALF = 16,
+    M10 = 17,
+    M5 = 18,
+    MITIS = 12,  // NEW alwas on
+    MOCLOCK = 19,
+    MQUARTER = 20,
+    MTWENTY = 13,     // was 22
+    EXTRA = 21,       // new
+  };
 
-// Drive all pins LOW (open-collector active) output used as mosfet. Use INPUT mode as low states output
-void ocDriveLowAll_fullON() {  // called by interrupt
-  for (size_t lo = 0; lo < totOutputPins; lo++) {
-    const size_t pin = lo;
-    if (values[pin] > 0) {
-      pinMode(pin, OUTPUT);
-      digitalWrite(pin, LOW);
-    } else {
-      pinMode(pin, INPUT);
-      //digitalWrite(pin, LOW); // dummy
+  WordClock() : totOutputPins(22) {
+    values = new size_t[totOutputPins];
+  }
+
+  ~WordClock() {
+    delete[] values;
+  }
+
+  void testLed() {
+    for (size_t p = 0; p < totOutputPins; p++) {
+      for (size_t lo = 0; lo < totOutputPins; lo++) {
+        const size_t pin = lo;
+        if (lo == p) {
+          values[pin] = 1;  //[pin] = 1;
+        } else {
+          values[pin] = 0;  //[pin] = 0;
+        }
+      }
+      delay(1000);
     }
   }
-}
 
-void ocDriveLowAll_fullOFF() {  // called by interrupt
-  for (size_t lo = 0; lo < totOutputPins; lo++) {
-    pinMode(lo, INPUT);  // OFF State
-  }
-}
-
-// chop led current obsolete since taken cared by interrupts
-void ocDriveWithoutInterrupt(size_t cycles = 10) {
-  for (size_t t = 0; t < cycles; t++) {
+  // Drive all pins LOW (open-collector active) output used as mosfet. Use INPUT mode as low states output
+  void ocDriveLowAll_fullON() {  // called by interrupt
     for (size_t lo = 0; lo < totOutputPins; lo++) {
-      pinMode(lo, INPUT);  // OFF State
-    }
-    delay(10);
-    for (size_t lo = 0; lo < totOutputPins; lo++) {
-      if (values[lo] > 0) {
-        pinMode(lo, OUTPUT);
-        digitalWrite(lo, LOW);
+      const size_t pin = lo;
+      if (values[pin] > 0) {
+        pinMode(pin, OUTPUT);
+        digitalWrite(pin, LOW);
       } else {
-        pinMode(lo, INPUT);
+        pinMode(pin, INPUT);
         //digitalWrite(pin, LOW); // dummy
       }
     }
-    delay(1);
   }
-  for (size_t lo = 0; lo < totOutputPins; lo++) {
-    pinMode(lo, INPUT);  // OFF State
+
+  void ocDriveLowAll_fullOFF() {  // called by interrupt
+    for (size_t lo = 0; lo < totOutputPins; lo++) {
+      pinMode(lo, INPUT);  // OFF State
+    }
   }
-}
 
-
-
-
-void setWordClock(const int curMin, const int curHourtrue, const bool writeItIs = true) {
-  values[(size_t)WordGP::MITIS] = writeItIs ? 1 : 0;
-  int curHour = curHourtrue;  // displayed
-  if (curMin > 34) curHour++;
-
-  if (curMin < 5) values[(size_t)WordGP::MOCLOCK] = 1;
-  else values[(size_t)WordGP::MOCLOCK] = 0;
-  values[(size_t)WordGP::M5] = 0;
-  values[(size_t)WordGP::M10] = 0;
-  values[(size_t)WordGP::MQUARTER] = 0;
-  values[(size_t)WordGP::MTWENTY] = 0;
-  // past / to
-  if ((curMin >= 5) && (curMin < 35)) values[(size_t)WordGP::HPAST] = 1;
-  else values[(size_t)WordGP::HPAST] = 0;
-  if ((curMin >= 35) && (curMin < 60)) values[(size_t)WordGP::HTO] = 1;
-  else values[(size_t)WordGP::HTO] = 0;
-  // min
-  if ((curMin >= 0) && (curMin < 5)) values[(size_t)WordGP::MOCLOCK] = 1;
-  else values[(size_t)WordGP::MOCLOCK] = 0;
-  if ((curMin >= 5) && (curMin < 10)) values[(size_t)WordGP::M5] = 1;
-  if ((curMin >= 10) && (curMin < 15)) values[(size_t)WordGP::M10] = 1;
-  if ((curMin >= 15) && (curMin < 20)) values[(size_t)WordGP::MQUARTER] = 1;
-  if ((curMin >= 20) && (curMin < 25)) values[(size_t)WordGP::MTWENTY] = 1;
-  if ((curMin >= 25) && (curMin < 30)) {
-    values[(size_t)WordGP::MTWENTY] = 1;
-    values[(size_t)WordGP::M5] = 1;
+  // chop led current obsolete since taken cared by interrupts
+  void ocDriveWithoutInterrupt(size_t cycles = 10) {
+    for (size_t t = 0; t < cycles; t++) {
+      for (size_t lo = 0; lo < totOutputPins; lo++) {
+        pinMode(lo, INPUT);  // OFF State
+      }
+      delay(10);
+      for (size_t lo = 0; lo < totOutputPins; lo++) {
+        if (values[lo] > 0) {
+          pinMode(lo, OUTPUT);
+          digitalWrite(lo, LOW);
+        } else {
+          pinMode(lo, INPUT);
+          //digitalWrite(pin, LOW); // dummy
+        }
+      }
+      delay(1);
+    }
+    for (size_t lo = 0; lo < totOutputPins; lo++) {
+      pinMode(lo, INPUT);  // OFF State
+    }
   }
-  if ((curMin >= 30) && (curMin < 35)) values[(size_t)WordGP::HALF] = 1;
-  else values[(size_t)WordGP::HALF] = 0;
-  if ((curMin >= 35) && (curMin < 40)) {
-    values[(size_t)WordGP::MTWENTY] = 1;
-    values[(size_t)WordGP::M5] = 1;
+
+
+
+
+  void setWordClock(const int curMin, const int curHourtrue, const bool writeItIs = true) {
+    values[(size_t)WordGP::MITIS] = writeItIs ? 1 : 0;
+    int curHour = curHourtrue;  // displayed
+    if (curMin > 34) curHour++;
+
+    if (curMin < 5) values[(size_t)WordGP::MOCLOCK] = 1;
+    else values[(size_t)WordGP::MOCLOCK] = 0;
+    values[(size_t)WordGP::M5] = 0;
+    values[(size_t)WordGP::M10] = 0;
+    values[(size_t)WordGP::MQUARTER] = 0;
+    values[(size_t)WordGP::MTWENTY] = 0;
+    // past / to
+    if ((curMin >= 5) && (curMin < 35)) values[(size_t)WordGP::HPAST] = 1;
+    else values[(size_t)WordGP::HPAST] = 0;
+    if ((curMin >= 35) && (curMin < 60)) values[(size_t)WordGP::HTO] = 1;
+    else values[(size_t)WordGP::HTO] = 0;
+    // min
+    if ((curMin >= 0) && (curMin < 5)) values[(size_t)WordGP::MOCLOCK] = 1;
+    else values[(size_t)WordGP::MOCLOCK] = 0;
+    if ((curMin >= 5) && (curMin < 10)) values[(size_t)WordGP::M5] = 1;
+    if ((curMin >= 10) && (curMin < 15)) values[(size_t)WordGP::M10] = 1;
+    if ((curMin >= 15) && (curMin < 20)) values[(size_t)WordGP::MQUARTER] = 1;
+    if ((curMin >= 20) && (curMin < 25)) values[(size_t)WordGP::MTWENTY] = 1;
+    if ((curMin >= 25) && (curMin < 30)) {
+      values[(size_t)WordGP::MTWENTY] = 1;
+      values[(size_t)WordGP::M5] = 1;
+    }
+    if ((curMin >= 30) && (curMin < 35)) values[(size_t)WordGP::HALF] = 1;
+    else values[(size_t)WordGP::HALF] = 0;
+    if ((curMin >= 35) && (curMin < 40)) {
+      values[(size_t)WordGP::MTWENTY] = 1;
+      values[(size_t)WordGP::M5] = 1;
+    }
+    if ((curMin >= 40) && (curMin < 45)) values[(size_t)WordGP::MTWENTY] = 1;
+    if ((curMin >= 45) && (curMin < 50)) values[(size_t)WordGP::MQUARTER] = 1;
+    if ((curMin >= 50) && (curMin < 55)) values[(size_t)WordGP::M10] = 1;
+    if ((curMin >= 55) && (curMin < 60)) values[(size_t)WordGP::M5] = 1;
+    // hours
+    if ((curHour == 0) || (curHour == 12) || (curHour == 24)) values[(size_t)WordGP::H12] = 1;
+    else values[(size_t)WordGP::H12] = 0;
+    if ((curHour == 1) || (curHour == 13)) values[(size_t)WordGP::H1_] = 1;
+    else values[(size_t)WordGP::H1_] = 0;
+    if ((curHour == 2) || (curHour == 14)) values[(size_t)WordGP::H2_] = 1;
+    else values[(size_t)WordGP::H2_] = 0;
+    if ((curHour == 3) || (curHour == 15)) values[(size_t)WordGP::H3_] = 1;
+    else values[(size_t)WordGP::H3_] = 0;
+    if ((curHour == 4) || (curHour == 16)) values[(size_t)WordGP::H4_] = 1;
+    else values[(size_t)WordGP::H4_] = 0;
+    if ((curHour == 5) || (curHour == 17)) values[(size_t)WordGP::H5_] = 1;
+    else values[(size_t)WordGP::H5_] = 0;
+    if ((curHour == 6) || (curHour == 18)) values[(size_t)WordGP::H6_] = 1;
+    else values[(size_t)WordGP::H6_] = 0;
+    if ((curHour == 7) || (curHour == 19)) values[(size_t)WordGP::H7_] = 1;
+    else values[(size_t)WordGP::H7_] = 0;
+    if ((curHour == 8) || (curHour == 20)) values[(size_t)WordGP::H8_] = 1;
+    else values[(size_t)WordGP::H8_] = 0;
+    if ((curHour == 9) || (curHour == 21)) values[(size_t)WordGP::H9_] = 1;
+    else values[(size_t)WordGP::H9_] = 0;
+    if ((curHour == 10) || (curHour == 22)) values[(size_t)WordGP::H10] = 1;
+    else values[(size_t)WordGP::H10] = 0;
+    if ((curHour == 11) || (curHour == 23)) values[(size_t)WordGP::H11] = 1;
+    else values[(size_t)WordGP::H11] = 0;
+    // AM
+    // if ((curHourtrue < 12) || (curHourtrue == 24)) {values[AM] = 1; values[PM] = 0;} else {values[AM] = 0;values[PM] = 1;}
   }
-  if ((curMin >= 40) && (curMin < 45)) values[(size_t)WordGP::MTWENTY] = 1;
-  if ((curMin >= 45) && (curMin < 50)) values[(size_t)WordGP::MQUARTER] = 1;
-  if ((curMin >= 50) && (curMin < 55)) values[(size_t)WordGP::M10] = 1;
-  if ((curMin >= 55) && (curMin < 60)) values[(size_t)WordGP::M5] = 1;
-  // hours
-  if ((curHour == 0) || (curHour == 12) || (curHour == 24)) values[(size_t)WordGP::H12] = 1;
-  else values[(size_t)WordGP::H12] = 0;
-  if ((curHour == 1) || (curHour == 13)) values[(size_t)WordGP::H1_] = 1;
-  else values[(size_t)WordGP::H1_] = 0;
-  if ((curHour == 2) || (curHour == 14)) values[(size_t)WordGP::H2_] = 1;
-  else values[(size_t)WordGP::H2_] = 0;
-  if ((curHour == 3) || (curHour == 15)) values[(size_t)WordGP::H3_] = 1;
-  else values[(size_t)WordGP::H3_] = 0;
-  if ((curHour == 4) || (curHour == 16)) values[(size_t)WordGP::H4_] = 1;
-  else values[(size_t)WordGP::H4_] = 0;
-  if ((curHour == 5) || (curHour == 17)) values[(size_t)WordGP::H5_] = 1;
-  else values[(size_t)WordGP::H5_] = 0;
-  if ((curHour == 6) || (curHour == 18)) values[(size_t)WordGP::H6_] = 1;
-  else values[(size_t)WordGP::H6_] = 0;
-  if ((curHour == 7) || (curHour == 19)) values[(size_t)WordGP::H7_] = 1;
-  else values[(size_t)WordGP::H7_] = 0;
-  if ((curHour == 8) || (curHour == 20)) values[(size_t)WordGP::H8_] = 1;
-  else values[(size_t)WordGP::H8_] = 0;
-  if ((curHour == 9) || (curHour == 21)) values[(size_t)WordGP::H9_] = 1;
-  else values[(size_t)WordGP::H9_] = 0;
-  if ((curHour == 10) || (curHour == 22)) values[(size_t)WordGP::H10] = 1;
-  else values[(size_t)WordGP::H10] = 0;
-  if ((curHour == 11) || (curHour == 23)) values[(size_t)WordGP::H11] = 1;
-  else values[(size_t)WordGP::H11] = 0;
-  // AM
-  // if ((curHourtrue < 12) || (curHourtrue == 24)) {values[AM] = 1; values[PM] = 0;} else {values[AM] = 0;values[PM] = 1;}
-}
 
-void debugSetHoursLeds(int unitDigit = 0) {
-  for (size_t i = 0; i < totOutputPins; i++) {
-    values[i] = 0;
+  void debugSetHoursLeds(int unitDigit = 0) {
+    for (size_t i = 0; i < totOutputPins; i++) {
+      values[i] = 0;
+    }
+    if (unitDigit == 1) { values[(size_t)WordGP::H1_] = 1; }
+    if (unitDigit == 2) { values[(size_t)WordGP::H2_] = 1; }
+    if (unitDigit == 3) { values[(size_t)WordGP::H3_] = 1; }
+    if (unitDigit == 4) { values[(size_t)WordGP::H4_] = 1; }
+    if (unitDigit == 5) { values[(size_t)WordGP::H5_] = 1; }
+    if (unitDigit == 6) { values[(size_t)WordGP::H6_] = 1; }
+    if (unitDigit == 7) { values[(size_t)WordGP::H7_] = 1; }
+    if (unitDigit == 8) { values[(size_t)WordGP::H8_] = 1; }
+    if (unitDigit == 9) { values[(size_t)WordGP::H9_] = 1; }
+    if (unitDigit == 10) { values[(size_t)WordGP::H10] = 1; }
+    if (unitDigit == 11) { values[(size_t)WordGP::H11] = 1; }
+    if (unitDigit == 12) { values[(size_t)WordGP::H12] = 1; }
+    return;
   }
-  if (unitDigit == 1) { values[(size_t)WordGP::H1_] = 1; }
-  if (unitDigit == 2) { values[(size_t)WordGP::H2_] = 1; }
-  if (unitDigit == 3) { values[(size_t)WordGP::H3_] = 1; }
-  if (unitDigit == 4) { values[(size_t)WordGP::H4_] = 1; }
-  if (unitDigit == 5) { values[(size_t)WordGP::H5_] = 1; }
-  if (unitDigit == 6) { values[(size_t)WordGP::H6_] = 1; }
-  if (unitDigit == 7) { values[(size_t)WordGP::H7_] = 1; }
-  if (unitDigit == 8) { values[(size_t)WordGP::H8_] = 1; }
-  if (unitDigit == 9) { values[(size_t)WordGP::H9_] = 1; }
-  if (unitDigit == 10) { values[(size_t)WordGP::H10] = 1; }
-  if (unitDigit == 11) { values[(size_t)WordGP::H11] = 1; }
-  if (unitDigit == 12) { values[(size_t)WordGP::H12] = 1; }
-  return;
-}
 
+};
 
+WordClock theWordClock;
 
 /// @brief listen to analogic input of DCF77 antenna and saves time with the listen method
 class DCF77Decoder {
@@ -622,7 +648,7 @@ public:
       if (oldIndexSec != indexSec) {
         fClockControl.adjustTime(seconds);
         oldIndexSec = indexSec;
-        if (debug2) debugSetHoursLeds(10);
+        if (debug2) theWordClock.debugSetHoursLeds(10);
 
         // manage validity of time set by initCountDownValidTime
         if (countDownValidTime > 0) countDownValidTime -= 1;
@@ -661,8 +687,8 @@ public:
         bool displayStrongTimeWhileReceptingSignal = false;
         if (displayStrongTimeWhileReceptingSignal) {
           time_t t = now();
-          setWordClock(minute(t), hour(t), fClockControl.isReliable());
-          ocDriveLowAll_fullON();
+          theWordClock.setWordClock(minute(t), hour(t), fClockControl.isReliable());
+          theWordClock.ocDriveLowAll_fullON();
         }
       }
       // DOWN -> UP
@@ -679,7 +705,7 @@ public:
           const size_t pointerInArrayMinus = (indexSec + 59) % 60;
           setRaw(pointerInArrayMinus, 3);
 
-          if (debug2) debugSetHoursLeds(4);
+          if (debug2) theWordClock.debugSetHoursLeds(4);
           setStart(pointerInArrayMinus);
 
           if (areAllOK()) {
@@ -741,14 +767,14 @@ public:
               int deltaDUR = 0;
               String pulse = "";
               if (durUpMili < 150) {  // short pulse
-                if (debug2) debugSetHoursLeds(8);
+                if (debug2) theWordClock.debugSetHoursLeds(8);
                 //valueIndexSec[indexSec] = 0; DEL
                 setRaw(indexSec, 0);
 
                 deltaDUR = durUpMili - 100;
                 pulse = "S";
               } else {
-                if (debug2) debugSetHoursLeds(7);
+                if (debug2) theWordClock.debugSetHoursLeds(7);
                 setRaw(indexSec, 1);
                 //valueIndexSec[indexSec] = 1; DEL
                 deltaDUR = durUpMili - 200;
@@ -1039,24 +1065,17 @@ void setup() {
     NULL,
     &timer10ms);
 #endif
-  if (debug2) debugSetHoursLeds(1);
+  if (debug2) theWordClock.debugSetHoursLeds(1);
 
   DBG_BEGIN(115200);
   delay(2000);
 
-  analogReadResolution(12);
-
+  analogReadResolution(12); // for DCF77 pin
   DBG_PRINT("DCF77 pin : ");
   DBG_PRINTLN(RADIOINPUT);
 
   // Start with everything released
-  ocDriveLowAll_fullOFF();
-
-  for (size_t lo = 0; lo < totOutputPins; lo++) {
-    const size_t pin = lo;
-    values[pin] = 0;
-  }
-
+  theWordClock.ocDriveLowAll_fullOFF();
 
   // setting up Led
   pinMode(LEDPIN, OUTPUT);
@@ -1069,31 +1088,19 @@ void setup() {
   digitalWrite(LEDPIN, LOW);
 
   DBG_PRINTLN("End setup");
-  if (debug2) debugSetHoursLeds(2);
+  if (debug2) theWordClock.debugSetHoursLeds(2);
 }
 
-void testLed() {
-  for (size_t p = 0; p < totOutputPins; p++) {
-    for (size_t lo = 0; lo < totOutputPins; lo++) {
-      const size_t pin = lo;
-      if (lo == p) {
-        values[pin] = 1;  //[pin] = 1;
-      } else {
-        values[pin] = 0;  //[pin] = 0;
-      }
-    }
-    delay(1000);
-  }
-}
+
 
 
 void loop() {
-  if (debug2) debugSetHoursLeds(3);
+  if (debug2) theWordClock.debugSetHoursLeds(3);
 
   // Test each output led
   const bool testEachLedFirst = false;
   if (testEachLedFirst) { 
-    testLed();
+    theWordClock.testLed();
   }
 
   for (int superLoop = 0; superLoop < 100000000; superLoop++) {
@@ -1109,7 +1116,7 @@ void loop() {
     DBG_PRINT(numberMinStaysInLoop);
     DBG_PRINTLN(" min.");
     time_t t = now();
-    setWordClock(minute(t), hour(t), dcf77.isClockReliable());
+    theWordClock.setWordClock(minute(t), hour(t), dcf77.isClockReliable());
     int lastMin = minute(t);
     for (unsigned long long loo = 0UL; loo < 1000000000; loo++) {
       t = now();
@@ -1120,7 +1127,7 @@ void loop() {
         // refine time if cristal not precise enough
         dcf77.adjustClockTimeMinute(millis() / 60000LL);
         digitalWrite(LEDPIN, ((curMin % 2) == 0) ? LOW : HIGH);
-        setWordClock(minute(t), hour(t), dcf77.isClockReliable());
+        theWordClock.setWordClock(minute(t), hour(t), dcf77.isClockReliable());
         numberMinStaysInLoop -= 1;
         if (numberMinStaysInLoop <= 0) break;
         SLEEPORDELAYMS(55000);  // waits for 55 sec
